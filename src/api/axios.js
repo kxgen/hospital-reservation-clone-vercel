4a736caa815df7dc51b65b1dcb00226e9ee5df56
Kibru <positivevibes.kg@gmail.com>
2026-02-19 18:31:36 +0300

    fix: axios.js http to https

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://trinity.runasp.net',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://trinity.runasp.net',
   headers: {
     'Content-Type': 'application/json',
   },
